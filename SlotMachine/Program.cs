f45363ccb6babe243fc7c95b708e39d025f4a6b7
Kela <kelaise2@gmail.com>
2025-03-02 22:59:47 -0500

    Display grid after  the user's bet

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -39,16 +39,7 @@ namespace SlotMachine
                 return;
             }
 
-            for (int rows = 0; rows < SLOT_ROWS; rows++) // iterate through rows & columns to assign & print random numbers
-            {
-                for (int cols = 0; cols < SLOT_COLUMNS; cols++)
-                {
-                    slotGrid[rows, cols] = random.Next(LOWER_LIMIT, UPPER_LIMIT + UPPER_LIMIT_INCREMENTER);
-                    Console.Write(slotGrid[rows, cols] + " ");
-
-                }
-                Console.WriteLine();
-            }
+         
 
             budget = budget - wager;// Deduct wager from budget
 
@@ -107,6 +98,17 @@ namespace SlotMachine
                         break;
                     }
                 }
+            }
+
+            for (int rows = 0; rows < SLOT_ROWS; rows++) // iterate through rows & columns to assign & print random numbers
+            {
+                for (int cols = 0; cols < SLOT_COLUMNS; cols++)
+                {
+                    slotGrid[rows, cols] = random.Next(LOWER_LIMIT, UPPER_LIMIT + UPPER_LIMIT_INCREMENTER);
+                    Console.Write(slotGrid[rows, cols] + " ");
+
+                }
+                Console.WriteLine();
             }
 
             if (checkWin)
